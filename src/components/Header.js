9c8276fc22f64ec0c308fe443903868f4867852c
parkjongjoon7128 <whdwnsdk8111@naver.com>
2023-05-12 16:51:26 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../designs/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>test</h1>
+      <button onClick={""}>버튼</button>
+    </header>
+  );
+};
+
+export default Header;
